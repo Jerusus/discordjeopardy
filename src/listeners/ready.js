ed02d682c4a752b6492e0cd22f179565d0e66758
kdcheng10 <kdcheng10@gmail.com>
2020-03-21 13:04:27 -0500

    Build a leaderboard

--- src/listeners/ready.js
+++ src/listeners/ready.js
@@ -9,13 +9,12 @@ module.exports = class Ready extends Listener {
 
   exec() {
     console.log(
-      `DiscordJeopardy started. ${this.client.guilds.size} guilds, ${
-        this.client.channels.size
-      } channels, and ${this.client.users.size} users.`
+      `DiscordJeopardy started. ${this.client.guilds.size} guilds, ${this.client.channels.size} channels, and ${this.client.users.size} users.`
     );
-    this.client.user.setGame(`${this.client.users.size} users`);
-    setInterval(() => {
-      this.client.user.setGame(`${this.client.users.size} users`);
-    }, 600000);
+    // this.client.user.setGame(`${this.client.users.size} users`);
+    // setInterval(() => {
+    //   this.client.user.setGame(`${this.client.users.size} users`);
+    // }, 600000);
+    this.client.user.setActivity(`NEW leaderboard! Type "t.top"`);
   }
 };
