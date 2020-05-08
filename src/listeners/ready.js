07bd3669df27ff4b8201003e4f21d602225ba136
kdcheng10 <kdcheng10@gmail.com>
2020-05-08 18:16:31 -0500

    update discord display status

--- src/listeners/ready.js
+++ src/listeners/ready.js
@@ -11,9 +11,13 @@ module.exports = class Ready extends Listener {
     console.log(
       `DiscordJeopardy started. ${this.client.guilds.size} guilds, ${this.client.channels.size} channels, and ${this.client.users.size} users.`
     );
-    this.client.user.setGame(`${this.client.users.size} users`);
+    this.client.user.setGame(
+      `${this.client.users.size.toLocaleString()} users`
+    );
     setInterval(() => {
-      this.client.user.setGame(`${this.client.users.size} users`);
+      this.client.user.setGame(
+        `${this.client.users.size.toLocaleString()} users`
+      );
     }, 600000);
   }
 };
