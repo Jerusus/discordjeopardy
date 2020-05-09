0947f54747617665981f5feee39a51c9c5e8b450
kdcheng10 <kdcheng10@gmail.com>
2020-05-09 00:03:53 -0500

    leaderboard refactor and cache

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -8,7 +8,8 @@ AWS.config.update({
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-const recentGuild = new Set();
+let dbCache;
+let cacheFresh = false;
 
 class LeaderboardCommand extends Command {
   constructor() {
@@ -20,17 +21,6 @@ class LeaderboardCommand extends Command {
 
   exec(message) {
     if (!message.guild) return;
-    if (recentGuild.has(message.guild.id)) {
-      message.channel.send(
-        'Leaderboard displayed too recently. Try again later...'
-      );
-      return;
-    } else {
-      recentGuild.add(message.guild.id);
-      setTimeout(() => {
-        recentGuild.delete(message.guild.id);
-      }, constants.leaderboardCooldown);
-    }
     const guildMembers = message.guild.members.array();
     var scores = [];
     var userMap = {};
@@ -41,42 +31,57 @@ class LeaderboardCommand extends Command {
     var scanParams = {
       TableName: 'PlayerData'
     };
-    docClient.scan(scanParams, function(err, data) {
-      if (err) {
-        console.error(
-          'Unable to read item. Error JSON:',
-          JSON.stringify(err, null, 2)
-        );
-      } else {
-        for (let item of data.Items) {
-          if (!(item.UserId in userMap)) {
-            continue;
-          }
-          var curScoreObject = new ScoreObject(
-            item.UserId,
-            userMap[item.UserId],
-            item.Score
+    if (!cacheFresh) {
+      console.log('Scanning db...');
+      docClient.scan(scanParams, function(err, data) {
+        if (err) {
+          console.error(
+            'Unable to read item. Error JSON:',
+            JSON.stringify(err, null, 2)
           );
-          scores.push(curScoreObject);
+        } else {
+          cacheFresh = true;
+          setTimeout(() => {
+            cacheFresh = false;
+          }, constants.leaderboardCooldown);
+
+          displayScore(data, userMap, message);
         }
-        console.log(scores);
-        scores.sort((a, b) => {
-          return b.score - a.score;
-        });
-        var msg = '**Here are the top scores on this server:**\n```\n';
-        msg += '+----------------------------------------------------+\n';
-        for (var i = 0; i < 5 && i < scores.length; i++) {
-          var rank = i + 1;
-          msg += `| ${rank}. ${scores[i].username.padEnd(34)}${(
-            '$' + scores[i].score.toLocaleString()
-          ).padStart(13) + ' |\n'}`;
-        }
-        msg += '+----------------------------------------------------+\n';
-        msg += '```';
-        message.channel.send(msg);
-      }
-    });
+      });
+    } else {
+      console.log('Using db cache...');
+      displayScore(dbCache, userMap, message);
+    }
   }
+}
+
+function displayScore(dbData, userMap, message) {
+  for (let item of data.Items) {
+    if (!(item.UserId in userMap)) {
+      continue;
+    }
+    var curScoreObject = new ScoreObject(
+      item.UserId,
+      userMap[item.UserId],
+      item.Score
+    );
+    scores.push(curScoreObject);
+  }
+  console.log(scores);
+  scores.sort((a, b) => {
+    return b.score - a.score;
+  });
+  var msg = '**Here are the top scores on this server:**\n```\n';
+  msg += '+----------------------------------------------------+\n';
+  for (var i = 0; i < 5 && i < scores.length; i++) {
+    var rank = i + 1;
+    msg += `| ${rank}. ${scores[i].username.padEnd(34)}${(
+      '$' + scores[i].score.toLocaleString()
+    ).padStart(13) + ' |\n'}`;
+  }
+  msg += '+----------------------------------------------------+\n';
+  msg += '```';
+  message.channel.send(msg);
 }
 
 function ScoreObject(userId, username, score) {
