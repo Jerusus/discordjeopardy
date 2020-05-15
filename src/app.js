357aff0efcfc326a0882ba4e487899dbb021f8b4
kdcheng10 <kdcheng10@gmail.com>
2020-05-14 20:48:07 -0500

    top.gg integration

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const { AkairoClient } = require('discord-akairo');
+const DBL = require('dblapi.js');
 const constants = require('./constants');
 
 const client = new AkairoClient(
@@ -7,15 +8,25 @@ const client = new AkairoClient(
     prefix: constants.prefix,
     allowMention: true,
     commandDirectory: './src/commands/',
-    listenerDirectory: './src/listeners/'
+    listenerDirectory: './src/listeners/',
   },
   {
-    disableEveryone: true
+    disableEveryone: true,
   }
 );
 
 client.login(process.env.TOKEN).then(() => {
   console.log('Logged in!');
+});
+
+const dbl = new DBL(process.env.TOPGG_TOKEN, client);
+// Optional events
+dbl.on('posted', () => {
+  console.log('Server count posted!');
+});
+
+dbl.on('error', (e) => {
+  console.log(`Oops! ${e}`);
 });
 
 setTimeout(() => {
