43cfefbedeab9271c62e9e9fb459536cb918b767
kdcheng10 <kdcheng10@gmail.com>
2020-05-15 00:14:55 -0500

    testing new daily command

--- src/app.js
+++ src/app.js
@@ -2,6 +2,16 @@ const Discord = require('discord.js');
 const { AkairoClient } = require('discord-akairo');
 const DBL = require('dblapi.js');
 const constants = require('./constants');
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+  console.log(req);
+  console.log(res);
+});
 
 const client = new AkairoClient(
   {
@@ -19,7 +29,11 @@ client.login(process.env.TOKEN).then(() => {
   console.log('Logged in!');
 });
 
-const dbl = new DBL(process.env.TOPGG_TOKEN, client);
+const dbl = new DBL(
+  process.env.TOPGG_TOKEN,
+  { webhookPort: process.env.PORT, webhookAuth: process.env.TOPGG_WEBHOOKAUTH },
+  client
+);
 
 dbl.on('posted', () => {
   console.log('Server count posted!');
@@ -29,11 +43,11 @@ dbl.on('error', (e) => {
   console.log(`Oops! ${e}`);
 });
 
-// dbl.webhook.on('ready', (hook) => {
-//   console.log(
-//     `Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`
-//   );
-// });
+dbl.webhook.on('ready', (hook) => {
+  console.log(
+    `Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`
+  );
+});
 
 setTimeout(() => {
   client.ws.connection.triggerReady();
--- src/commands/daily.js
+++ src/commands/daily.js
@@ -1,27 +1,27 @@
-const { Command } = require('discord-akairo');
-const DBL = require('dblapi.js');
-const constants = require('../constants');
-const AWS = require('aws-sdk');
+// const { Command } = require('discord-akairo');
+// const DBL = require('dblapi.js');
+// const constants = require('../constants');
+// const AWS = require('aws-sdk');
 
-AWS.config.update({
-  region: 'us-west-2',
-});
+// AWS.config.update({
+//   region: 'us-west-2',
+// });
 
-const docClient = new AWS.DynamoDB.DocumentClient();
+// const docClient = new AWS.DynamoDB.DocumentClient();
 
-// const dbl = new DBL(process.env.TOPGG_TOKEN, this.client);
+// // const dbl = new DBL(process.env.TOPGG_TOKEN, this.client);
 
-class DailyCommand extends Command {
-  constructor() {
-    super('daily', {
-      aliases: constants.dailyAliases,
-      channelRestriction: 'guild',
-    });
-  }
+// class DailyCommand extends Command {
+//   constructor() {
+//     super('daily', {
+//       aliases: constants.dailyAliases,
+//       channelRestriction: 'guild',
+//     });
+//   }
 
-  exec(message) {
-    console.log('Daily...');
-  }
-}
+//   exec(message) {
+//     console.log('Daily...');
+//   }
+// }
 
-module.exports = DailyCommand;
+// module.exports = DailyCommand;
