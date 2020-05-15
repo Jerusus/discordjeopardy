f9b13398241a836f45879ab1fa2c8f76710c2949
kdcheng10 <kdcheng10@gmail.com>
2020-05-15 10:17:56 -0500

    newrelic

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
+require('newrelic');
 const Discord = require('discord.js');
 const { AkairoClient } = require('discord-akairo');
 const DBL = require('dblapi.js');
 const constants = require('./constants');
 const AWS = require('aws-sdk');
-require('newrelic');
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('ping');
+});
 
 const tableName = 'PlayerData';
 AWS.config.update({
