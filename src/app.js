a3625965da51aafaf832eb15800762aa153de5c6
kdcheng10 <kdcheng10@gmail.com>
2020-11-06 21:36:37 -0600

    revert intents

--- src/app.js
+++ src/app.js
@@ -37,9 +37,6 @@ class DiscordJeopardyClient extends AkairoClient {
         ownerID: '175374170815725569',
       },
       {
-        ws: { intents: ['GUILD_MEMBERS', 'GUILD_MESSAGES', 'DIRECT_MESSAGES'] },
-      },
-      {
         disableMentions: 'everyone',
       }
     );
