abd69c58406727a09a52572707ded4c2aa7b9666
kdcheng10 <kdcheng10@gmail.com>
2021-04-11 21:06:27 -0500

    sharding

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const { ShardingManager } = require('discord.js');
 
-const manager = new ShardingManager('bot.js', {
+const manager = new ShardingManager('../bot.js', {
     totalShards: 'auto',
     token: process.env.TOKEN
 });
